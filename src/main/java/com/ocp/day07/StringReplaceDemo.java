61d1f45174b1eb91a5f2bd2cdade5a7dabd1080a
MB-study <MB-study@192.168.1.194>
2021-03-30 19:42:20 +0800

    JAVA1020

--- src/main/java/com/ocp/day07/StringReplaceDemo.java
+++ src/main/java/com/ocp/day07/StringReplaceDemo.java
@@ -13,11 +13,11 @@ public class StringReplaceDemo {
     public static void main(String[] args) {
         String s1="Jbvb";
         String s2="Java";
-        s1=s2.replace('b', 'a');
+        s1=s1.replace('b', 'a');//replace之後，記憶體新建一字串，源自串不會改變。再做s1=s1，指向新字串
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s1==s2);
-        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s2));        
         
     }
 }
