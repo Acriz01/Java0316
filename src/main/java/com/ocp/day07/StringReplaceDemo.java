52d2a91ca9bbd5f6622c8bb83f3090315498a353
MB-study <MB-study@192.168.1.194>
2021-03-30 19:19:02 +0800

    JAVA1020

--- src/main/java/com/ocp/day07/StringReplaceDemo.java
+++ src/main/java/com/ocp/day07/StringReplaceDemo.java
@@ -9,18 +9,15 @@ package com.ocp.day07;
  *
  * @author MB-study
  */
-public class StringEquals3 {
+public class StringReplaceDemo {
     public static void main(String[] args) {
-        String s1="Java";
+        String s1="Jbvb";
         String s2="Java";
-        System.out.println("s1==s2");
-        s1=s1+"8";
-        s2=s2.concat("8");//作用與上述相等，即連接一個"8"(字串相連)
+        s1=s2.replace('b', 'a');
         System.out.println(s1);
         System.out.println(s2);
-        System.out.println(s1==s2);//false
-        System.out.println(s1.intern()==s2.intern());//true
-        //字串比較盡量用equals不要用雙等號(有可能錯誤)
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+        
     }
- 
 }
