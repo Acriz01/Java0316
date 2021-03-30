a186ccad8c3a6fe1aa55c2f946997e040cfb6271
MB-study <MB-study@192.168.1.194>
2021-03-30 19:00:06 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day07/StringEquals.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ocp.day07;
+
+/**
+ *
+ * @author MB-study
+ */
+public class StringEquals {
+    public static void main(String[] args) {
+        String s1=new String("Java");
+        String s2=new String("");
+    }
+}
