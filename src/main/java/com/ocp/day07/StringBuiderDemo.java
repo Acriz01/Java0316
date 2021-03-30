e5ae892b031b5521727fe869572f790987743713
MB-study <MB-study@192.168.1.194>
2021-03-30 20:00:25 +0800

    JAVA1020

--- src/main/java/com/ocp/day07/StringBuiderDemo.java
+++ src/main/java/com/ocp/day07/StringBuiderDemo.java
@@ -12,12 +12,18 @@ package com.ocp.day07;
 public class StringBuiderDemo {
     public static void main(String[] args) {
         StringBuilder sb=new StringBuilder("吃葡萄吐葡萄");
+        //吃葡萄不吐葡萄皮
         System.out.println(sb);
         sb.append("皮");
         System.out.println(sb);
         sb.insert(3, "不");
         System.out.println(sb);
         sb.replace(1, 2, "葡");
+        System.out.println(sb);
+        //吐葡萄
+        sb.delete(0, 4);
+        System.out.println(sb);
+        sb.delete(3, sb.length());
         System.out.println(sb);
     }
 }
