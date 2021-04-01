f5dc546642ebfdb63e43b697efeab25003f7d68c
MB-study <MB-study@192.168.1.194>
2021-04-01 21:46:23 +0800

    JAVA1020

--- src/main/java/com/ocp/day08/MultiArrayDemo4.java
+++ src/main/java/com/ocp/day08/MultiArrayDemo4.java
@@ -10,14 +10,16 @@ package com.ocp.day08;
  * @author MB-study
  */
 public class MultiArrayDemo4 {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         //三人的身高體重如下，求BMI
-        double[][] hw={{170,160,180},{60,45,90}};
-        double bmi;
+        double[][] hw = {{170, 160, 180}, {60, 45, 90}};
+         double bmi;
         for(int i=0;i<3;i++){
             bmi=hw[1][i]/Math.pow((hw[0][i]/100),2);//體重除以身高(公尺)的平方
             System.out.printf("BMI%d=%.2f\n",i,bmi);
         }
-        
+
     }
 }
+
