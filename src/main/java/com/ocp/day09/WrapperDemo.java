eb93f376182926133b868487ea6c57c9aae2648c
MB-study <MB-study@192.168.1.194>
2021-04-06 18:59:08 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day09/WrapperDemo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ocp.day09;
+
+/**
+ *
+ * @author MB-study
+ */
+public class WrapperDemo {
+    public static void main(String[] args) {
+        int max=Integer.MAX_VALUE;
+        System.out.printf("int 的最大值:%d\n",max);
+        
+    }
+}
