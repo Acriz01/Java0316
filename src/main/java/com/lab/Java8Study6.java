aed8c93d56af6504e23802efd54c3367e8ab6557
MB-study <MB-study@192.168.1.194>
2021-04-06 21:24:22 +0800

    JAVA1020

--- src/main/java/com/lab/Java8Study6.java
+++ src/main/java/com/lab/Java8Study6.java
@@ -5,19 +5,27 @@
  */
 package com.lab;
 
+import java.util.stream.Stream;
+
 /**
  *
  * @author MB-study
  */
 public class Java8Study6 {
+
     public static void main(String[] args) {
-        String[] names={"banana","watermalon","apple","orange","mango"};
+        String[] names = {"banana", "watermalon", "apple", "orange", "mango"};
         //求所有字母總和個數
-        int sum=0;
-        for(String n:names){
-            sum+=n.length();
+        int sum = 0;
+        for (String n : names) {
+            sum += n.length();
         }
-            System.out.println(sum);
-        
+        System.out.println(sum);
+        //求字母為偶數個的字串，字母總和個數
+        int sum2=Stream.of(names)
+                .mapToInt(String::length)//.mapToInt(s->s.length())
+                .filter(s->s%2==0)
+                .sum();
+        System.out.println(sum2);
     }
 }
