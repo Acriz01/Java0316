aec4f28f288da39bbaf4855e9b4264fa042f4fff
MB-study <MB-study@192.168.1.194>
2021-04-06 19:23:03 +0800

    JAVA1020

--- src/main/java/com/ocp/day09/WrapperDemo.java
+++ src/main/java/com/ocp/day09/WrapperDemo.java
@@ -12,7 +12,14 @@ package com.ocp.day09;
 public class WrapperDemo {
     public static void main(String[] args) {
         int max=Integer.MAX_VALUE;
+        int min=Integer.MIN_VALUE;
         System.out.printf("int 的最大值:%d\n",max);
+        int max2=Integer.max(10, 20);
+        System.out.printf("10與20的最大值是:%d\n",max2);
+        String chinese="20";
+        String english="0";
+        System.out.printf("總分:%s\n",chinese+english);
+        System.out.printf("總分:%s\n",Integer.parseInt(chinese)+Integer.parseInt(english));
         
     }
 }
