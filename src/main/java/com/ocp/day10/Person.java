1833e4fa31fc819547f0719ce74c5cabff5a4180
MB-study <MB-study@192.168.1.194>
2021-04-08 19:40:46 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day10/Person.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ocp.day10;
+
+/**
+ *
+ * @author MB-study
+ */
+public class Person {
+    //物見屬性
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+    //建構子
+  
+    public Person(String name, int age, double height, double weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+        //物件方法(此時若屬封裝)
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
+    
+}
