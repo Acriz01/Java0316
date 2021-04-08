dd35e0b0b110e1d851cda29d911b982f7b1b1693
MB-study <MB-study@192.168.1.194>
2021-04-08 21:13:38 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day10/PersonMain4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ocp.day10;
+
+/**
+ *
+ * @author MB-study
+ */
+public class PersonMain4 {
+    public static void main(String[] args) {
+        Person[][] people = {
+            {//A班
+            new Person("John", 17, 170, 60.5),
+            new Person("Mary", 18, 160, 50.5)
+            },{//B班
+            new Person("Jo", 19, 155, 42.5),
+            new Person("Helen", 16, 185, 75.5),
+            new Person("Tom", 21, 190, 120.5)
+            }
+        };
+        //請印出所有人的bmi資料
+        
+    }
+}
