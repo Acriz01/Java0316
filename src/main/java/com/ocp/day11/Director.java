98b82b8584f5d539275bbe1e5e81384a266da319
MB-study <MB-study@192.168.1.194>
2021-04-13 19:13:01 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day11/Director.java
@@ -0,0 +1,15 @@
+
+package com.ocp.day11;
+
+public class Director extends Manager{
+    private int stockOption;
+    
+    public int getStockOption() {
+        return stockOption;
+    }
+
+    public void setStockOption(int stockOption) {
+        this.stockOption = stockOption;
+    }
+    
+}
---
+++ src/main/java/com/ocp/day11/Manager.java
@@ -0,0 +1,15 @@
+
+package com.ocp.day11;
+
+public class Manager extends Employee {
+    private int budget;
+    
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+    
+}
