f77b296fb383a63f8948ec227e5db036df4ea078
MB-study <MB-study@192.168.1.194>
2021-04-15 21:07:50 +0800

    JAVA1020

--- src/main/java/com/ocp/day12/OverloadingDemo.java
+++ src/main/java/com/ocp/day12/OverloadingDemo.java
@@ -2,6 +2,12 @@
 package com.ocp.day12;
 //超載，或稱之為負載
 public class OverloadingDemo {
+    public void calc(short x){
+        System.out.println("short: "+x);
+    }
+     public void calc(long x){
+        System.out.println("long: "+x);
+    }
     public void calc(int x){
         System.out.println("int: "+x);
     }
@@ -14,7 +20,7 @@ public class OverloadingDemo {
     public void calc(double x){
         System.out.println("double: "+x);
     }
-    
+    //默認int32bits，之後先看比它大的再看比它小的，整數>符點數
     public static void main(String[] args) {
         OverloadingDemo o=new OverloadingDemo();
         o.calc(100);
