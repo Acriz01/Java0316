c0185031a96eef0e4163282108c7b74e7c7cd16b
MB-study <MB-study@192.168.1.194>
2021-04-20 21:32:04 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day15/Airplane.java
@@ -0,0 +1,7 @@
+
+package com.ocp.day15;
+
+public abstract class Airplane {
+    abstract int speed();
+    
+}
---
+++ src/main/java/com/ocp/day15/Truck.java
@@ -0,0 +1,11 @@
+
+package com.ocp.day15;
+
+public class Truck extends Car{
+
+    @Override
+    void move() {
+        System.out.println("用輪胎走");
+    }
+    
+}
