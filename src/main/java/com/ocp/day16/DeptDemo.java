34229c3af25f5d4d7c40751b7c67af4ddcfc3116
MB-study <MB-study@192.168.1.194>
2021-04-22 19:12:37 +0800

    JAVA1020

--- src/main/java/com/ocp/day16/DeptDemo.java
+++ src/main/java/com/ocp/day16/DeptDemo.java
@@ -6,5 +6,10 @@ public class DeptDemo {
         Dept dept=new Dept();
         Dept.Staff staff=dept.new Staff();
         staff.job();
+        //第二種寫法
+        Dept.Staff staff2=new Dept().new Staff();
+        staff2.job();
+        //第三種寫法
+        new Dept().new Staff().job();
     }
 }
