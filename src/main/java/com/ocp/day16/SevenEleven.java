1c074650d93a7b03ba0960462de483f08cead4e7
MB-study <MB-study@192.168.1.194>
2021-04-22 19:37:05 +0800

    JAVA1020

--- src/main/java/com/ocp/day16/SevenEleven.java
+++ src/main/java/com/ocp/day16/SevenEleven.java
@@ -2,5 +2,21 @@
 package com.ocp.day16;
 
 public class SevenEleven {
-    
+    //結帳方法
+    public void submit(int price){
+        //結帳類別
+        class Transaction{
+            //交易資料
+            void print(){
+                System.out.printf("交易金額: %d\n",price);
+            }
+        }
+        Transaction t=new Transaction();
+        t.print();
+    }
+    //main獨立於上面結構之外
+    public static void main(String[] args) {
+        SevenEleven se=new SevenEleven();
+        se.submit(100);
+    }
 }
