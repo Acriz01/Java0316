cece0fd1ba9e53cd376d3b97002fc3f6ff4ef3fd
MB-study <MB-study@192.168.1.194>
2021-04-29 20:57:26 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day18/School.java
@@ -0,0 +1,11 @@
+
+package com.ocp.day18;
+
+public class School {
+    public static void main(String[] args) {
+        Arrays.stream(DataCenter.getPeople())
+                .forEach(System.out::println);
+        //請問考試的總分與平均=?
+        
+    }
+}
---
+++ src/main/java/com/ocp/day18/Student.java
@@ -0,0 +1,41 @@
+package com.ocp.day18;
+
+public class Student extends Person {
+    private int score;
+private Teacher teacher;
+
+    public Student() {
+    }
+
+    public Student(int score, Teacher teacher, String name, int age) {
+        super(name, age);
+        this.score = score;
+        this.teacher = teacher;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "score=" + score + ", teacher=" + teacher + '}';
+    }
+    
+    
+
+
+
+}
