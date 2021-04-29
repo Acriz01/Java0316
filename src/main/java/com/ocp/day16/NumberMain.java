7967a736285f7a61d553a77d7838ba66c751e1f2
MB-study <MB-study@192.168.1.194>
2021-04-29 19:22:42 +0800

    JAVA1020

--- src/main/java/com/ocp/day16/NumberMain.java
+++ src/main/java/com/ocp/day16/NumberMain.java
@@ -1,18 +1,11 @@
-
 package com.ocp.day16;
 
 public class NumberMain {
     public static void main(String[] args) {
-        //Lamda實作EvenNumber =50是否是偶數
-        EvenNumber even=(n)->n%2==0;
+        // Lambda 實作 EvenNumber, n = 50 是否是偶數
+        EvenNumber even = (n) -> n % 2 == 0;
         System.out.println(even.isEven(50));
-        //Lamda實作PrimeNumber =9973是否是質數
-        PrimeNumber prime=(n)->{
-            for(int i=1;i<=n;i++){
-                
-            }
-
-        };
-       
+        // Lambda 實作 PrimeNumber, n = 9973 是否是質數
+        
     }
 }
--- src/main/java/com/ocp/day17/FunctionDemo.java
+++ src/main/java/com/ocp/day17/FunctionDemo.java
@@ -1,9 +1,12 @@
 
 package com.ocp.day17;
 
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
 public class FunctionDemo {
+    public static void main(String[] args) { 
     //利用Function來實作圓面積
     //半徑r是整數，面積area是浮點數
     Function<Integer,Double> f1=(Integer r)->Math.pow(r,2)*Math.PI;
@@ -13,5 +16,10 @@ public class FunctionDemo {
     Function<Integer,Double> f2=(Integer r)->4.0/3.0*Math.pow(r, 3);
     System.out.println(f2.apply(10));
     //利用BiFunction來求矩形面積
-    //半徑r是整數，面積area是浮點數
+    BiFunction<Integer,Integer,Integer> f3=(h,w)->h*w;
+    System.out.println(f3.apply(10,20));
+    //利用BiFunction來求矩形面積
+    BinaryOperator<Integer> f4=(h,w)->h*w;//=BiFunction<T,T,T>，https://docs.oracle.com/javase/8/docs/api/java/util/function/BinaryOperator.html
+    System.out.println(f4.apply(10,20));
+    }
 }
