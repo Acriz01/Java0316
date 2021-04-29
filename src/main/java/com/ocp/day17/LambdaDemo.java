5448dee2b3084765f9b2f9bf9ffdf0d12d5f29aa
MB-study <MB-study@192.168.1.194>
2021-04-29 19:44:19 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day17/LambdaDemo.java
@@ -0,0 +1,16 @@
+
+package com.ocp.day17;
+
+import java.util.stream.Stream;
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        Stream.of(100,50,70,80,30)
+                //.peek(x->System.out.println(x.getClass().getTypeName()))//Consumer
+                .forEach(x->System.out.println(x));//Consumer
+        
+        Stream.of(100,50,70,80,30)
+                .filter(x->x>=60)//Predicate
+                .forEach(System.out::println);//Consumer
+    }
+}
