a1dfbade214373dac0916caa380ffe7bcd337cd7
MB-study <MB-study@192.168.1.194>
2021-05-04 21:34:16 +0800

    JAVA1020

--- src/main/java/com/ocp/day22/Lotto.java
+++ src/main/java/com/ocp/day22/Lotto.java
@@ -2,7 +2,7 @@ package com.ocp.day22;
 
 import java.util.Arrays;
 import java.util.Random;
-
+//private建構子singleton單一實體<->Multiton多例(就是平常可以new很多個的)
 public class Lotto {
     private String value;
     private static Lotto lottoInstance=new Lotto();//樂透本身自己建自己(因為別人不能new)
@@ -24,3 +24,16 @@ public class Lotto {
         return value;
     }
 }
+/*
+考題:
+You want to create a singleton class by using the Singleton design pattern.
+Which two statements enforce the singleton nature of the design?
+
+A. Make the class static.
+B. Make the constructor private.
+C. Override equals() and hashCode() methods of the java.lang.Object class.
+D. Use a static reference to point to the single instance.
+E. Implement the Serializable interface.
+
+Ans:BD
+*/
