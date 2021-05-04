b42a67c71770d16e9b3d6c87f76bd1789f1306a9
MB-study <MB-study@192.168.1.194>
2021-05-04 19:31:08 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day20/Pen.java
@@ -0,0 +1,19 @@
+
+package com.ocp.day20;
+
+public class Pen {
+    private String color;
+    private int price;
+
+    public Pen(String color, int price) {
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Pen{" + "color=" + color + ", price=" + price + '}';
+    }
+    
+    
+}
--- src/main/java/com/ocp/day20/StringEqualsDemo.java
+++ src/main/java/com/ocp/day20/StringEqualsDemo.java
@@ -5,10 +5,16 @@ public class StringEqualsDemo {
     public static void main(String[] args) {
         String s1="Java";
         String s2=new String("Java");
-        String s3=new String(new char[]{'j','a','v','a'});
+        String s3=new String(new char[]{'J','a','v','a'});
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
+        System.out.println(s1.equals(s2));
+        System.out.println(s2.equals(s3));//==比較記憶體位置
+        //物件型別使用==的時機， a與b是否指向同一物件
+        String a="Java";
+        String b=a;//b指向a的位置
+        System.out.println(a==b);
     }
    
 }
