9a8a16b31eb0b7d39e0026eaf63c4f89d00842a6
MB-study <MB-study@192.168.1.194>
2021-05-04 21:23:56 +0800

    JAVA1020

--- src/main/java/com/ocp/day22/Lotto.java
+++ src/main/java/com/ocp/day22/Lotto.java
@@ -1,19 +1,26 @@
-
 package com.ocp.day22;
 
 import java.util.Arrays;
 import java.util.Random;
 
-
 public class Lotto {
-    public String getValues(){
-        Random r=new Random();
-        int[] values={
-            r.nextInt(9)+1,
-            r.nextInt(9)+1,
-            r.nextInt(9)+1,
-            r.nextInt(9)+1
+    private String value;
+    private static Lotto lottoInstance=new Lotto();//樂透本身自己建自己(因為別人不能new)
+    public static Lotto getLotto(){
+        return lottoInstance;
+    }
+    private Lotto() {
+        Random r = new Random();
+        int[] values = {
+            r.nextInt(9) + 1,
+            r.nextInt(9) + 1,
+            r.nextInt(9) + 1,
+            r.nextInt(9) + 1
         };
-        return Arrays.toString(values);
+        value = Arrays.toString(values);
+    }
+
+    public String getValue() {
+        return value;
     }
 }
