565ef872152a49f74132bc1a67a15f77a8e424e7
MB-study <MB-study@192.168.1.194>
2021-05-06 21:04:59 +0800

    JAVA1020

--- src/main/java/com/ocp/day23/SetDemo4.java
+++ src/main/java/com/ocp/day23/SetDemo4.java
@@ -1,4 +1,3 @@
-
 package com.ocp.day23;
 
 import java.util.IntSummaryStatistics;
@@ -6,8 +5,9 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetDemo4 {
+
     public static void main(String[] args) {
-         Set<Integer> scores = new LinkedHashSet<>();
+        Set<Integer> scores = new LinkedHashSet<>();
         scores.add(100);
         scores.add(2000);
         scores.add(90);
@@ -15,8 +15,11 @@ public class SetDemo4 {
         scores.add(-150);
         System.out.println(scores);
         //請用java8計算國英數的總數
-        int sum=0;
-        sum=scores.stream().mapToInt(Integer::intValue).filter(score->score=<100&&score>=0).sum();
-        
+        int sum = scores.stream()
+                .filter(Score::isValid)
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sum);
+
     }
 }
