94fdb695ec0f36ac9e27255ab8f2d0f17c525d0a
MB-study <MB-study@192.168.1.194>
2021-05-06 19:11:53 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day23/SetDemo1.java
@@ -0,0 +1,16 @@
+
+package com.ocp.day23;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo1 {
+    public static void main(String[] args) {
+        Set subjects=new HashSet();
+        subjects.add("國");
+        subjects.add("英");
+        subjects.add("數");
+        System.out.println(subjects);
+        
+    }
+}
