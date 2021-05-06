addd46542113c991f69184eefe5d93f14b13616a
MB-study <MB-study@192.168.1.194>
2021-05-06 19:17:09 +0800

    JAVA1020

--- src/main/java/com/ocp/day23/SetDemo1.java
+++ src/main/java/com/ocp/day23/SetDemo1.java
@@ -2,11 +2,12 @@
 package com.ocp.day23;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetDemo1 {
     public static void main(String[] args) {
-        Set subjects=new HashSet();
+        Set subjects=new LinkedHashSet();
         subjects.add("國");
         subjects.add("英");
         subjects.add("數");
