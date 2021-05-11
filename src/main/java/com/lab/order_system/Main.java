b1bec6d83cce11eb4f01847310666cba8a9d34ba
MB-study <MB-study@192.168.1.194>
2021-05-11 21:16:06 +0800

    JAVA1020

--- src/main/java/com/lab/order_system/Main.java
+++ src/main/java/com/lab/order_system/Main.java
@@ -1,17 +1,51 @@
-
 package com.lab.order_system;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class Main {
-    private static List<Food> order=new ArrayList<>();
-    private static DataCenter dc=new DataCenter();
-    private static ServiceCenter sc=new ServiceCenter();
-    
+
+    private static List<Food> order = new ArrayList<>();
+    private static DataCenter dc = new DataCenter();
+    private static ServiceCenter sc = new ServiceCenter();
+    private static boolean play = true;
+
     public static void main(String[] args) {
-        menu();
+        while (play) {
+            menu();
+        }
     }
-    public static void menu(){}
+
+    public static void menu() {
+        sc.menu();
+        System.out.print("請輸入:");
+        Scanner scanner = new Scanner(System.in);
+        int itemId = scanner.nextInt();
+        switch (itemId) {
+            case 1:
+                sc.printCatalog();
+                break;
+            case 2:
+                System.out.print("請輸入No: ");
+                int addNo = scanner.nextInt();
+                //在order集合中加入Food
+                Food food = sc.getFoodByNo(addNo);
+                order.add(food);
+                break;
+            case 3:
+                for (Food f : order) {
+                    System.out.println(f);
+                }
+                break;
+            case 4://取消訂單餐點
+                break;
+            case 5://刪除訂單餐點
+                break;
+            case 6://結帳
+                break;
+            case 0://離開
+                play = false;
+        }
+    }
 }
