8239e59049b31a226bcdf0d03252a3036ad76a24
MB-study <MB-study@192.168.1.194>
2021-05-11 21:27:34 +0800

    JAVA1020

--- src/main/java/com/lab/order_system/Main.java
+++ src/main/java/com/lab/order_system/Main.java
@@ -5,46 +5,52 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
     private static List<Food> order = new ArrayList<>();
     private static DataCenter dc = new DataCenter();
     private static ServiceCenter sc = new ServiceCenter();
     private static boolean play = true;
-
+    
     public static void main(String[] args) {
-        while (play) {
+        while (play) {            
             menu();
         }
     }
-
+    
     public static void menu() {
         sc.menu();
-        System.out.print("請輸入:");
+        System.out.print("請輸入: ");
         Scanner scanner = new Scanner(System.in);
         int itemId = scanner.nextInt();
-        switch (itemId) {
-            case 1:
+        switch(itemId) {
+            case 1: // 檢視 Catalog
                 sc.printCatalog();
                 break;
-            case 2:
+            case 2: // 新增訂單餐點
                 System.out.print("請輸入No: ");
                 int addNo = scanner.nextInt();
-                //在order集合中加入Food
+                // 在 order 集合中加入 Food
                 Food food = sc.getFoodByNo(addNo);
                 order.add(food);
                 break;
-            case 3:
-                for (Food f : order) {
-                    System.out.println(f);
+            case 3: // 查詢訂單餐點
+                for(int i=0;i<order.size();i++) {
+                    System.out.printf("id: %d %s\n", i, order.get(i));
                 }
                 break;
-            case 4://取消訂單餐點
+            case 4: // 取消訂單餐點
+                System.out.print("請輸入取消No: ");
+                int cancelId = scanner.nextInt();
+                order.remove(cancelId); // 移除單筆元素
                 break;
-            case 5://刪除訂單餐點
+            case 5: // 刪除全部訂單
+                order = new ArrayList<>();
                 break;
-            case 6://結帳
+            case 6: // 結帳
+                int sum = order.stream().mapToInt(Food::getPrice).sum();
+                System.out.printf("結帳: %d\n", sum);
+                order = new ArrayList<>();
                 break;
-            case 0://離開
+            case 0: // 離開
                 play = false;
         }
     }
