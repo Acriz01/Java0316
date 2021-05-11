e82d83179644a99374389f86944d2e6a72071386
MB-study <MB-study@192.168.1.194>
2021-05-11 21:06:58 +0800

    JAVA1020

--- src/main/java/com/lab/order_system/ServiceCenter.java
+++ src/main/java/com/lab/order_system/ServiceCenter.java
@@ -1,20 +1,44 @@
-
 package com.lab.order_system;
 
 import java.util.Set;
 
 public class ServiceCenter {
-private Set<Food> catalog=new DataCenter().getCatalog();
+
+    private Set<Food> catalog = new DataCenter().getCatalog();
+//根據no來查找Food
+
+    public Food getFoodByNo(int no) {
+        return catalog.stream()
+                .filter(f -> f.getNo() == no)
+                .findFirst()
+                .get();
+    }
 //檢視Catalog
-    public void printCatalog(){
+
+    public void printCatalog() {
         System.out.println("訂單系統");
-        System.out.println("------------");
-        for(Food food:catalog){
-            System.out.printf("編號:%2d 品名:%s 尺寸:%c 價格:%3d \n",
-                        food.getNo(),food.getName(),food.getSize(),food.getPrice()
-                    );
+        System.out.println("+-----------------------+");
+        System.out.println("|NO|　品名　|尺寸|Price|");
+        System.out.println("+-----------------------+");
+        for (Food food : catalog) {
+            System.out.printf("|%2d|%s|　%c|%5d| \n",
+                    food.getNo(), food.getName(), food.getSize(), food.getPrice()
+            );
         }
-        System.out.println("------------");
-        
+        System.out.println("+-----------------------+");
+
+    }
+    //檢視系統menu
+    public void menu(){
+        System.out.println("系統項目");
+        System.out.println("+-----------------------+");
+        System.out.println("1.　檢視 Catalog");
+        System.out.println("2.　新增訂單餐點");
+        System.out.println("3.　查詢訂單餐點");
+        System.out.println("4.　取消訂單餐點");
+        System.out.println("5.　刪除全部訂單");
+        System.out.println("6.　結帳");
+        System.out.println("7.　離開");
+        System.out.println("+-----------------------+");
     }
 }
