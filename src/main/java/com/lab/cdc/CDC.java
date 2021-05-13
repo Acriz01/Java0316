8191beb220bc6a97aff44170adcd28fbd58023b5
MB-study <MB-study@192.168.1.194>
2021-05-13 21:12:12 +0800

    JAVA1020

--- src/main/java/com/lab/cdc/CDC.java
+++ src/main/java/com/lab/cdc/CDC.java
@@ -8,6 +8,8 @@ public class CDC {
     private String alert_disease;
     private String areaDesc;
     private String circle;
+//加值資料
+    private double distance;//距離
 
     public String getSent() {
         return sent;
@@ -57,9 +59,17 @@ public class CDC {
         this.circle = circle;
     }
 
-    @Override
-    public String toString() {
-        return "CDC{" + "sent=" + sent + ", headline=" + headline + ", description=" + description + ", alert_disease=" + alert_disease + ", areaDesc=" + areaDesc + ", circle=" + circle + '}';
+    public double getDistance() {
+        return distance;
     }
 
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return "CDC{" + "sent=" + sent + ", headline=" + headline + ", description=" + description + ", alert_disease=" + alert_disease + ", areaDesc=" + areaDesc + ", circle=" + circle + ", distance=" + distance + '}';
+    }
+    
 }
---
+++ src/main/java/com/lab/rice/Util.java
@@ -0,0 +1,19 @@
+package com.lab.rice;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Util {
+
+    public static String getJson() throws Exception {
+        String path = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        URL url = new URL(path);
+        String json = new Scanner(new InputStreamReader(url.openStream(), "UTF-8"))
+                .useDelimiter("\\A")
+                .next();
+        return json;
+
+    }
+}
