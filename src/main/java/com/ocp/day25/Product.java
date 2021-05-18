27d91ab31f71dc423de2656a1f5d308132f5f74b
MB-study <MB-study@192.168.1.194>
2021-05-18 19:13:55 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day25/Product.java
@@ -0,0 +1,37 @@
+
+package com.ocp.day25;
+
+
+public class Product {
+    //商品
+    private String name;//品名
+    private int price;//價格
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "name=" + name + ", price=" + price + '}';
+    }
+    
+    
+}
