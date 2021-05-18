05ddc7dcf10b752eb2342c9c26e49e78b76edc79
MB-study <MB-study@192.168.1.194>
2021-05-18 21:34:28 +0800

    JAVA1020

---
+++ src/main/java/com/ocp/day26/MapGroupingByDemo2.java
@@ -0,0 +1,12 @@
+
+package com.ocp.day26;
+
+import java.util.Random;
+
+public class MapGroupingByDemo2 {
+    public static void main(String[] args) {
+        //1-9取100次，每一個號碼各取到幾次
+        int random = (int) (Math.random()*10);
+        System.out.println(random);
+                }
+}
