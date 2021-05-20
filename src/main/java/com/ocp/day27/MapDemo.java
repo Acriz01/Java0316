126a4f0da80111138a903be2ebcd7d3e03d99bc0
user <user@192.168.56.1>
2021-05-20 19:10:17 +0800

    java

--- src/main/java/com/ocp/day27/MapDemo.java
+++ src/main/java/com/ocp/day27/MapDemo.java
@@ -2,11 +2,12 @@
 package com.ocp.day27;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class MapDemo {
     public static void main(String[] args) {
-        Map map1=new HashMap();//TreeMap();//HashMap();
+        Map map1=new Hashtable();//TreeMap();//HashMap();
         map1.put("B",100);
         map1.put("C",80);
         map1.put("E",80);
