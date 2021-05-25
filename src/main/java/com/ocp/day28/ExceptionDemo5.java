5fe75020cec61f1b0e05ca62cb1613a4883ed1c6
user <user@192.168.56.1>
2021-05-25 21:01:26 +0800

    java

--- src/main/java/com/ocp/day28/ExceptionDemo5.java
+++ src/main/java/com/ocp/day28/ExceptionDemo5.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ocp.day28;
 
-/**
- *
- * @author user
- */
 public class ExceptionDemo5 {
-    
+    public static void main(String[] args) {
+        
+        int[] x = {0};
+        try {
+            int avg = 10 / x[0];
+            System.out.println(avg);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+            e.printStackTrace(System.out); // 印出詳細錯誤資料
+            return;
+        } finally {
+            System.out.println("程式結束: 這是一定要執行的");
+        }
+        
+        
+        
+    }
 }
