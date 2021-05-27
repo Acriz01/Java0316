1e45bb1f140c9981c4b29b62ea5b3fb05d031140
user <user@192.168.56.1>
2021-05-27 22:38:50 +0800

    java

--- src/main/java/com/ocp/day29_assert/AssertDemo.java
+++ src/main/java/com/ocp/day29_assert/AssertDemo.java
@@ -1,9 +1,15 @@
 package com.ocp.day29_assert;
+
 public class AssertDemo {
+
     public static void main(String[] args) {
-        int score=400;
-        assert(score>=0&&score<=100):"分數錯誤:"+score;
-        System.out.println(score>=60?"pass":"Fail");
+        int score = 400;
+        //1.設定Main class
+        //2.加上-ea
+        //3.找出問題後要把assert拔除
+        assert (score >= 0 && score <= 100) : "分數錯誤:" + score;
+
+        System.out.println(score >= 60 ? "pass" : "Fail");
     }
-    
+
 }
---
+++ src/main/java/com/ocp/day29_assert/Square.java
@@ -0,0 +1,8 @@
+package com.ocp.day29_assert;
+public class Square {
+    //計算梯形面積
+    public int getArea(int top,int buttom,int h) {
+        int area=((top+buttom)*h)/2;
+        return area;
+    }
+}
